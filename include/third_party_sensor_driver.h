6e19d6c9686aada038f42038dcddcee931b4374a
JochiPochi <jaleman@bu.edu>
2018-04-20 07:53:06 -0400

    Add third party sensor driver to include
    This is a code stub and is just meant to help with testing

---
+++ include/third_party_sensor_driver.h
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+bool initialize(std::string serial_port_name)
+{
+  bool test = true;
+  return test;
+}
+
+std::string getTemp()
+{
+  std::string temp = "0x000639C0";
+  return (temp);
+}
+
+
+double getPressure()
+{
+  return (101.325);
+}
+
+uint32_t getValidity()
+{
+  uint32_t validity = 0xABABCDCD;
+  return validity;
+}
